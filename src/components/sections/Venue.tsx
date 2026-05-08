8e76bcf690637c9cab43149fc707334d0c92a848
Riqza AF <maulana.10125908@mahasiswa.unikom.ac.id>
2026-05-08 09:34:55 +0700

    tes bg

--- src/components/sections/Venue.tsx
+++ src/components/sections/Venue.tsx
@@ -65,7 +65,7 @@ const Venue = () => {
         {/* Judul "Venue" */}
         <FadeIn delay={0.2}>
           <p
-            className="absolute w-full text-center text-white z-10 pt-2 "
+            className="absolute w-full text-center text-white z-10 pt-2 bg-black "
             style={{ top: venueTop, fontFamily: "Cylburn, cursive", fontSize: 96}}
           >
             Venue
